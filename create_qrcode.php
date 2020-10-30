7680acefad8888ab49ef6e1800a16b350f315cca
Teun van der Ploeg <29253@ma-web.nl>
2020-10-30 11:38:11 +0100

    working
    working qrcode creator and reader

--- create_qrcode.php
+++ create_qrcode.php
@@ -1,6 +1,6 @@
 <?php
 // Bestand met functions inladen, deze regel laten staan aub
-require './includes/functions.php';
+require __DIR__ . "/vendor/autoload.php";
 
 // Laad de composer autoloader in
 
@@ -12,3 +12,19 @@ require './includes/functions.php';
 // Tip: Gebruik de SvgImageBackEnd()
 
 // Sla de QR code op als afbeelding: "qrcode.svg"
+
+use CodeItNow\BarcodeBundle\Utils\QrCode;
+
+$qrCode = new QrCode();
+$qrCode
+    ->setText('https://google.com')
+    ->setSize(300)
+    ->setPadding(10)
+    ->setErrorCorrection('high')
+    ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
+    ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
+    ->setLabel('Scan mijn QR code')
+    ->setLabelFontSize(16)
+    ->setImageType(QrCode::IMAGE_TYPE_PNG)
+;
+$qrCode->save('qrcode.png');
--- read_qrcode.php
+++ read_qrcode.php
@@ -1,15 +1,11 @@
 <?php
 // Laad de composer autoloader in
+require __DIR__ . "/vendor/autoload.php";
 
-// Lees de documentatie van de khanamiryan/qrcode-detector-decoder package:
-// Zo weet je hoe je deze package kunt gebruiken
-// https://github.com/khanamiryan/php-qrcode-detector-decoder/
+//use Zxing\QrReader;
 
-// Lees de QR code die je net hebt gemaakt
-// Het bestand moet eerst naar PNG omgezet worden, dat gebeurt hier
-// Deze code krijg je van mij en kun je laten staan.
+use Zxing\QrReader;
+$qrcode = new QrReader('qrcode.png');
+$text = $qrcode->text(); 
 
-
-// Je hebt NU dus een "qrcode.png" die je kunt inladen
-
-// Zet de tekst op het scherm die in de QR Code zit!
+echo "n\QR CODE = ". $text . "\n\n";
